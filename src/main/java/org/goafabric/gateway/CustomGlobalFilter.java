9fde2601b0276c9b65030c931825a6607da2a55a
Andreas Mautsch <amautsch>
2023-07-15 18:41:36 +0200

    changes

--- src/main/java/org/goafabric/gateway/CustomGlobalFilter.java
+++ src/main/java/org/goafabric/gateway/CustomGlobalFilter.java
@@ -3,11 +3,13 @@ package org.goafabric.gateway;
 
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.Authentication;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+
+import java.nio.file.AccessDeniedException;
 
 @Component
 public class CustomGlobalFilter {
@@ -15,11 +17,27 @@ public class CustomGlobalFilter {
     @Bean
     public GlobalFilter globalFilter() {
         return (exchange, chain) -> {
-            Mono<Authentication> auth = ReactiveSecurityContextHolder.getContext().map(ctx -> ctx.getAuthentication());
-            auth.subscribe(a ->
-                exchange.getRequest().mutate()
-                        .header("X-TenantId", ((OAuth2AuthenticationToken)a).getAuthorizedClientRegistrationId()).build());
-            return chain.filter(exchange);
+            return ReactiveSecurityContextHolder.getContext()
+                    .filter(c -> c.getAuthentication() != null)
+                    .flatMap(c -> {
+                        OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) c.getAuthentication();
+
+                        String tokenTenant = token.getAuthorizedClientRegistrationId();
+                        if (tokenTenant == null) {
+                            return Mono.error(
+                                    new AccessDeniedException("Invalid token. Tenant is not present in token.")
+                            );
+                        }
+
+                        ServerHttpRequest request = exchange.getRequest().mutate()
+                                .header("X-TenantId", tokenTenant).build();
+
+                        return chain.filter(exchange.mutate().request(request).build());
+                    })
+                    .switchIfEmpty(chain.filter(exchange));
+
         };
+
+
     }
 }
